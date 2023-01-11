85acc201077ca1ab4f9e578ed5383b934cf33789
AKASH VERMA <aakashverma4102@gmail.com>
2023-01-11 14:47:46 +0530

    adding database

--- app.js
+++ app.js
@@ -5,21 +5,32 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const { application } = require("express");
 const _=require("lodash");
+const mongoose= require("mongoose");
+
+
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+mongoose.set('strictQuery',false);
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB" ,{useNewUrlParser: true});
+
+
+const itemsSchema={
+  title: String,
+  content: String
+}
+const Item=mongoose.model('Item',itemsSchema);
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const app = express();
-
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
-
-const posts=[];
 app.get("/" ,(req,res)=> {
-    res.render('home',{startingContent:homeStartingContent,allPosts:posts});
+  Item.find((err,items)=>{
+    res.render('home',{startingContent:homeStartingContent,allPosts:items});
+  })
 });
 
 app.get("/about",(req,res)=>{
@@ -36,25 +47,27 @@ app.get("/compose",(req,res)=>{
 
 
 app.post("/compose",(req,res)=>{
-  const post={
+  const post= new Item({
     title:req.body.postTitle,
     content:req.body.postBody
-  };
-  posts.push(post);
-  res.redirect('/');
+  });
+  post.save(function(err){
+    if(!err){
+      res.redirect('/');
+    }
+  });
 });
 
 
-app.get("/post/:postName",(req,res)=>{
-  console.log(posts);
-  let requestedTitle=_.lowerCase(req.params.postName);
-  posts.forEach(function(post){
-    let storeTitle=_.lowerCase(post.title);
-    if(requestedTitle==storeTitle){
-        res.render('post',{heading:post.title ,startingContent:post.content});
-    } 
+app.get("/post/:postId",(req,res)=>{
+  let requestedPostId=req.params.postId;
+  Item.findOne({_id:requestedPostId},(err,post)=>{
+    res.render("post",{
+      heading:post.title,
+      startingContent:post.content
+    });
   });
-})
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
